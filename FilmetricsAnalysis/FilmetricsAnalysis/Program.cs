45fc45ad4746f939c234cd624f373b0f79cb6414
Lester Fan <hf155@scarletmail.rutgers.edu>
2016-08-04 12:37:07 -0400

    added test program

--- FilmetricsAnalysis/FilmetricsAnalysis/Program.cs
+++ FilmetricsAnalysis/FilmetricsAnalysis/Program.cs
@@ -12,8 +12,30 @@ namespace FilmetricsAnalysis
         {
             MicroscopeAnalyzer mAnalyzer = new MicroscopeAnalyzer();
 
-            mAnalyzer.AcquireBaseline();
-            if (mAnalyzer.mLastRet == 1) Console.WriteLine("Error!");
+            while(true)
+            {
+                Console.WriteLine("Enter in 'a' to take measurement, enter in 's' to save.");
+                string input = Console.ReadLine();
+                if (input == "a")
+                {
+                    Console.WriteLine("Now measuring!");
+                    mAnalyzer.Measure();
+                    if (mAnalyzer.mLastRet == 1) Console.WriteLine("Something went wrong in the last step!");
+                }
+                else if (input == "s")                                // Current file directory : C:\ProgramData\Filmetrics\Material
+                {
+                    Console.WriteLine("Please enter in the name of the file with extension .fmspe");
+                    Console.WriteLine("For now, all files will be saved in directory 'C:/ProgramData/Filmetrics/Material' ");
+                    string currFileDir = "C:/ProgramData/Filmetrics/Material/";
+                    string user_input = Console.ReadLine();
+
+                    currFileDir = currFileDir + user_input;
+
+                    mAnalyzer.SaveSpectrum(currFileDir);
+                    if (mAnalyzer.mLastRet == 1) Console.WriteLine("Something went wrong in the last step!");
+                }
+
+            }
         }
     }
 }
